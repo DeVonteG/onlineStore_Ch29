c6426e795b588ce69ddf9e0ab3705987e65a7cb4
DeVonte G <dagray92@hotmail.com>
2022-06-21 23:02:13 -0400

    Initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+function Footer(){
+    return(
+    <div className="footer">
+        <p>All copy rights reserved &copy; 2022 DeVonte </p>
+        {/* <h2>This will be the footer</h2> */}
+    </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+function Navbar(){
+    return (
+    <div className="navbar">
+        <h1>This will be the Menu! </h1> 
+    </div>
+    );
+}
+
+export default Navbar;
