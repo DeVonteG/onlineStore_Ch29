659f446292cc8e14907c4f7e9739ec48715c22e6
DeVonte G <dagray92@hotmail.com>
2022-07-10 14:03:45 -0400

    Changed punctuation

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,7 @@ const Home =()=>{
 
     return(
         <div>
-            <h1>Welcome to Orgnix, Fresh produce is only 1 click away</h1>
+            <h1>Welcome to Orgnix, Fresh produce is only 1 click away!!</h1>
             <Link className="btn btn-primary" aria-current="page" to="/catalog">Catalog</Link>
         </div>    
             
